5059b5dce053544b3774c850cc846cd2be80e881
taehwanlee203 <teein5579@naver.com>
2020-11-17 15:41:52 +0900

    [ADD] 메뉴 > 써브웨이 API 추가

--- js/script.js
+++ js/script.js
@@ -59,24 +59,28 @@ $(document).ready(function () {
     // menu 설명 보기
     var menu = $('#menu ul > li');
 
-    menu.on('mouseenter', function (event) {
-        var target = event.currentTarget;
+    const menuShow = (event) => {
+        menu.on('mouseenter', function (event) {
+            var target = event.currentTarget;
+    
+            $(target).find('.ko_title').stop().animate({ 'top': '50px' }, 400);
+            $(target).find('.en_title').stop().animate({ 'top': '95px' }, 400);
+            $(target).find('.desc').stop().animate({ 'top': '125px', 'opacity': '1' }, 500);
+            $(target).find('.icon').stop().animate({ 'bottom': '30px', 'opacity': '1' }, 300);
+        });
+    }
 
-        $(target).find('.ko_title').stop().animate({ 'top': '50px' }, 400);
-        $(target).find('.en_title').stop().animate({ 'top': '95px' }, 400);
-        $(target).find('.desc').stop().animate({ 'top': '125px', 'opacity': '1' }, 500);
-        $(target).find('.icon').stop().animate({ 'bottom': '30px', 'opacity': '1' }, 300);
-    });
-
-    menu.on('mouseleave', function (event) {
-        var target = event.currentTarget;
-
-        $(target).find('.ko_title').stop().animate({ 'top': '100px' });
-        $(target).find('.en_title').stop().animate({ 'top': '145px' });
-        $(target).find('.desc').stop().animate({ 'top': '200px', 'opacity': '0' });
-        $(target).find('.icon').stop().animate({ 'bottom': '100px', 'opacity': '0' });
-    });
-
+    const menuHide = (event) => {
+        menu.on('mouseleave', function (event) {
+            var target = event.currentTarget;
+    
+            $(target).find('.ko_title').stop().animate({ 'top': '100px' });
+            $(target).find('.en_title').stop().animate({ 'top': '145px' });
+            $(target).find('.desc').stop().animate({ 'top': '200px', 'opacity': '0' });
+            $(target).find('.icon').stop().animate({ 'bottom': '100px', 'opacity': '0' });
+        });
+    }
+    
     var menuTab = $('#menu-tab ul > li');
 
     menuTab.on('click', function (event) {
@@ -108,5 +112,77 @@ $(document).ready(function () {
         //ES5
         //ES6 - 요즘 쓰는 문법
     });
+
+    const getSandwich = () => {
+
+        return fetch('http://localhost:3000/subway/sandwich', {
+            'method' : 'GET',
+            'headers' : {
+                'Content-Type' : 'application/json'
+            }
+        }).then(res => res)
+        .then(res => res.json())
+    }
+    
+    const templateSandwichLabel = (label) => {
+        if(label) {
+            return `<div class="label">${label}</div>`;
+        } else {
+            return ``;
+        }
+    }
+
+    const templateSandwichKcal = (kcal) => {
+        if(kcal) {
+            return `<span class="kcal">${kcal}</span>`;
+        } else {
+            return ``;
+        }
+    }
+
+    const templateSandwich = (sandwich) => {
+        const {type, label, img, ko_title, en_title, kcal, summary, view_id} = sandwich;  // = const ko_title = sandwich['ko_titlle'];
+
+        return `
+        <li class="${type}">
+        <a href="#">
+            ${templateSandwichLabel(label)}
+            <div class="img">
+                <img src="${img}" alt="${ko_title}">
+            </div>
+            <strong class="ko_title">${ko_title}</strong>
+            <span class="en_title">${en_title}</span>
+            ${templateSandwichKcal(kcal)}
+            <p class="desc">${summary}</p>
+            <div class="icon" data-id="${view_id}"></div>
+        </a>
+        </li>
+        
+        `;
+    }
+
+    const listSandwich = async () => {
+        
+        const sandwiches = await getSandwich();
+
+        // sandwiches.then((data) => {
+        //     console.log(data)
+        // })
+
+        const menu = document.getElementById('menu');
+        const menuWrap = menu.querySelector('ul');
+
+
+        for (const sandwich of sandwiches) {
+            const node = $(templateSandwich(sandwich))[0];
+            $(node).on('mouseenter', menuShow);
+            $(node).on('mouseleave', menuHide);
+            menuWrap.append(node);
+
+        }
+
+    }
+
+    listSandwich();
 
 });
